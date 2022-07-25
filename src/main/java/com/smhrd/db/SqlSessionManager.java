debe0ccd65ff5e1cf2cbbd414ac5075cac7b30cc
smhrd <smhrd@220.80.165.4>
2022-07-25 15:45:07 +0900

    기본적인환경설정?_국예진

---
+++ src/main/java/com/smhrd/db/SqlSessionManager.java
@@ -0,0 +1,34 @@
+package com.smhrd.db;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class SqlSessionManager {
+
+
+		// static 블록 : 객체를 호출할 때 무조건 한번 실행되는 부분
+		// Ctrl+shift+O : 전체 import
+
+		static SqlSessionFactory sqlSessionFactory;
+		static {
+			try {
+				String resource = "com/smhrd/db/config.xml";
+				InputStream inputStream = Resources.getResourceAsStream(resource);
+				sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public static SqlSessionFactory getSqlSession() {
+			return sqlSessionFactory;
+		}
+
+
+	}
+
+
