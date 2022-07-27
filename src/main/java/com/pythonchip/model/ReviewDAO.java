816cd445af9711104ab07ef7f83d227472f2d821
smhrd <smhrd@59.0.124.93>
2022-07-27 12:20:27 +0900

    예약,리뷰 데이터 세팅_호영

--- src/main/java/com/pythonchip/model/ReviewDAO.java
+++ src/main/java/com/pythonchip/model/ReviewDAO.java
@@ -1,5 +1,14 @@
 package com.pythonchip.model;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.pythonchip.db.SqlSessionManager;
+
 public class ReviewDAO {
 
+	
+	private SqlSessionFactory sqlSessionFactory 
+	= SqlSessionManager.getSqlSession();
+
+	
 }
