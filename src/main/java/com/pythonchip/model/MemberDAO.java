60606a0b8e0ea28379d1bec033427a685b04c77a
smhrd <smhrd@211.227.224.245>
2022-07-29 10:50:07 +0900

    커밋푸시MemberDAO()성민

--- src/main/java/com/pythonchip/model/MemberDAO.java
+++ src/main/java/com/pythonchip/model/MemberDAO.java
@@ -1,5 +1,43 @@
 package com.pythonchip.model;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.pythonchip.db.SqlSessionManager;
+import com.pythonchip.model.MemberDTO;
+
 public class MemberDAO {
 
+	private SqlSessionFactory sqlsessionFactory
+	=SqlSessionManager.getSqlSession();
+
+// 회원가입
+public int join(MemberDTO dto) {
+	 
+	SqlSession session = sqlsessionFactory.openSession(true);
+	int row =session.insert("join",dto);
+	session.close();
+	
+	return row;
+}
+
+// 로그인
+public MemberDTO login(MemberDTO dto) {
+	SqlSession session =  sqlsessionFactory.openSession(true);
+	MemberDTO info =  session.selectOne("login",dto);
+	session.close();
+	
+	return info;
+		
+}
+
+public int update(MemberDTO dto) {
+	SqlSession session = sqlsessionFactory.openSession(true);
+	int row = session.update("update",dto);
+	session.close();
+	
+	return row;
+}
 }
