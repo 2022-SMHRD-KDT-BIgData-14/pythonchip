201d696f608a150835c31d8efb222f3f714edce9
smhrd <smhrd@211.227.224.245>
2022-07-29 10:44:01 +0900

    memberDTO

--- src/main/java/com/pythonchip/model/MemberDTO.java
+++ src/main/java/com/pythonchip/model/MemberDTO.java
@@ -1,5 +1,19 @@
 package com.pythonchip.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+
 public class MemberDTO {
 
+	@NonNull private String id;
+	@NonNull private String pw;
+	private String gender;
+	private String age;
+	private String user_tel;
 }
