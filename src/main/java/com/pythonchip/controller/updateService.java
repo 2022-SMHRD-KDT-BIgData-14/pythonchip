597ac491d92bf179b2b373e08b5eae2891c2080b
smhrd <smhrd@211.227.224.245>
2022-07-29 11:48:25 +0900

    MAIN:회원가입 로그인 짬뽕 udateService: 회원정보수정

---
+++ src/main/java/com/pythonchip/controller/updateService.java
@@ -0,0 +1,34 @@
+package com.pythonchip.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.pythonchip.command.Command;
+import com.pythonchip.model.MemberDAO;
+import com.pythonchip.model.MemberDTO;
+
+public class updateService implements Command{
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		String gender = request.getParameter("gender");
+		String age = request.getParameter("age");
+		String user_tel = request.getParameter("user_tel");
+		
+		MemberDTO dto = new MemberDTO(id,pw,gender,age,user_tel); 
+		int row = new MemberDAO().update(dto);
+		
+		if(row == -1) {
+			System.out.println("회원정보가 없습니다");
+		} else if(row==0) {
+			System.out.println("비밀번호 오류입니다.");
+		}else {
+			System.out.println("회원정보 수정 완료");
+			HttpSession session = request.getSession();
+			session.setAttribute("info", dto);
+		}
+		
+		return "./Main.jsp";
+	}
+}
